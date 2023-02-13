a4df22159e400f684dbec264236c8e3e85c1ffc8
G3rgosz <gergoszivak@gmail.com>
2023-02-13 12:10:06 +0100

    png -> pdf

--- src/App.java
+++ src/App.java
@@ -1,7 +1,10 @@
 
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.util.HashMap;
 import java.util.Map;
 import com.google.zxing.BarcodeFormat;
@@ -11,23 +14,29 @@ import com.google.zxing.NotFoundException;
 import com.google.zxing.WriterException;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
-import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+import com.itextpdf.awt.geom.Rectangle2D;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.pdf.PdfDocument;
+import com.itextpdf.text.pdf.PdfWriter;
+import com.itextpdf.text.Image;
+import com.itextpdf.text.PageSize;
+import com.itextpdf.text.Rectangle;
 
 public class App {
 	public static void main(String[] args) throws WriterException, IOException, NotFoundException{
 
-		String data = "teszt";
+		String data = "teszt22222222";
 		String path = "aztec.png";
 		String charset = "UTF-8";
 
-		Map<EncodeHintType, Object> hashMap = new HashMap<>(3);
+		Map<EncodeHintType, Object> hashMap = new HashMap<>(2);
 
-		//hashMap.put(EncodeHintType.ERROR_CORRECTION,1);
 		hashMap.put(EncodeHintType.AZTEC_LAYERS, 1);
 
-
-		createAZTEC(data, path, charset, hashMap, 1000, 1000);
+		createAZTEC(data, path, charset, hashMap, 925, 925);
 		System.out.println("Aztec-kód létrehozva! ;) ");
+		createPdf();
+
 	}
 
     public static void createAZTEC(String data, String path, String charset, Map hashMap,int height, int width) throws WriterException, IOException{
@@ -35,4 +44,22 @@ public class App {
 
         MatrixToImageWriter.writeToFile(matrix, path.substring(path.lastIndexOf('.') + 1), new File(path));
     }
+	
+	public static void createPdf()throws FileNotFoundException, MalformedURLException {
+		Document document = new Document(new Rectangle(1000,1000));
+		String input = "aztec.png";
+		String output = "aztec.pdf";
+		try {
+		  FileOutputStream fos = new FileOutputStream(output);
+		  PdfWriter writer = PdfWriter.getInstance(document, fos);
+		  writer.open();
+		  document.open();
+		  document.add(Image.getInstance(input));
+		  document.close();
+		  writer.close();
+		}
+		catch (Exception e) {
+		  e.printStackTrace();
+		}
+	}
 }
